560e61323c49199cc571b0d13e46aec1ec3596a7
Laurent Droguet <laurentdroguet@hotmail.fr>
2019-06-18 14:40:10 +0200

    added arrow on TimelineView

--- CombineMarbles/Features/Detail/TimelineView.swift
+++ CombineMarbles/Features/Detail/TimelineView.swift
@@ -10,13 +10,25 @@ import SwiftUI
 
 struct TimelineView : View {
     var body: some View {
-        GeometryReader {geometry in
-            Path(CGRect(x: 0,
-                        y: 0,
-                        width: geometry.size.width,
-                        height: 2))
-        }.padding()
-        .frame(height: 2)
+        GeometryReader { geometry in
+            self.addLineWithArrow(size: geometry.size)
+        }.padding(.trailing, 10)
+        .frame(height: 1)
+    }
+}
+
+extension TimelineView {
+    func addLineWithArrow(size: CGSize) -> Path {
+        var p = Path(CGRect(x: 0,
+                            y: 0,
+                            width: size.width,
+                            height: 1))
+        
+        p.move(to: CGPoint(x: size.width+10, y: 0.5))
+        p.addLine(to: CGPoint(x: size.width, y: -4))
+        p.addLine(to: CGPoint(x: size.width, y: 5))
+        
+        return p
     }
 }
 
